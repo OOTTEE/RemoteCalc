917d784e030565d8e32b6373ff78579354e2725b
OTE <javier.ote.ote@gmail.com>
2021-04-03 17:37:45 +0200

    Add ResponseFrame structure And Test

---
+++ polish-calc-api/src/main/java/me/ote/polishcalc/api/protocol/ResponseFrameHelper.java
@@ -0,0 +1,19 @@
+package me.ote.polishcalc.api.protocol;
+
+public class ResponseFrameHelper {
+
+    public ResponseFrame createResponse(Integer messageId, String payload) {
+        return null;
+    }
+
+    public ResponseFrame createFailResponse(Integer messageId) {
+        return null;    }
+
+    public ResponseFrame createErrorResponse(Integer messageId) {
+        return null;    }
+
+    public byte[] buildFrame(ResponseFrame responseFrame) {
+        return null;
+    }
+
+}
