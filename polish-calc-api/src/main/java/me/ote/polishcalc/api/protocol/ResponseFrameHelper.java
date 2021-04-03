1f51bd273f4c12e4c271b8be27b801cc53f036a3
OTE <javier.ote.ote@gmail.com>
2021-04-03 17:54:17 +0200

    Implement Response frame Helper

--- polish-calc-api/src/main/java/me/ote/polishcalc/api/protocol/ResponseFrameHelper.java
+++ polish-calc-api/src/main/java/me/ote/polishcalc/api/protocol/ResponseFrameHelper.java
@@ -1,19 +1,38 @@
 package me.ote.polishcalc.api.protocol;
 
+import java.nio.charset.StandardCharsets;
+
 public class ResponseFrameHelper {
 
+    public static final byte[] FAIL_PAYLOAD = {'F', 'A', 'I', 'L'};
+    public static final byte[] ERROR_PAYLOAD = {'E', 'R', 'R', 'O', 'R'};
+    public static final byte SEPARATOR = ';';
+    public static final byte END = '$';
+
     public ResponseFrame createResponse(Integer messageId, String payload) {
-        return null;
+        return new ResponseFrame(messageId, payload.getBytes(StandardCharsets.UTF_8));
     }
 
     public ResponseFrame createFailResponse(Integer messageId) {
-        return null;    }
+        return new ResponseFrame(messageId, FAIL_PAYLOAD);
+    }
 
     public ResponseFrame createErrorResponse(Integer messageId) {
-        return null;    }
+        return new ResponseFrame(messageId, ERROR_PAYLOAD);
+    }
 
     public byte[] buildFrame(ResponseFrame responseFrame) {
-        return null;
+        byte[] rawFrame = new byte[4 + responseFrame.getPayload().length];
+        rawFrame[0] = (byte) ((responseFrame.getMessageId() >> 8 ) & 0xFF);
+        rawFrame[1] = (byte) (responseFrame.getMessageId() & 0xFF);
+        rawFrame[2] = SEPARATOR;
+        if(responseFrame.getPayload() != null) {
+            for (int i = 0; i < responseFrame.getPayload().length; i++) {
+                rawFrame[3 + i] = responseFrame.getPayload()[i];
+            }
+        }
+        rawFrame[rawFrame.length-1] = END;
+        return rawFrame;
     }
 
 }
