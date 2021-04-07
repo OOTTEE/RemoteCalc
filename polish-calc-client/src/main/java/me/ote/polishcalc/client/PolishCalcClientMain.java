bc7040ff80b99a4b9fd8f455abd18bb19548d373
OTE <javier.ote.ote@gmail.com>
2021-04-08 00:34:46 +0200

    Create main client structure

--- polish-calc-client/src/main/java/me/ote/polishcalc/client/PolishCalcClientMain.java
+++ polish-calc-client/src/main/java/me/ote/polishcalc/client/PolishCalcClientMain.java
@@ -8,12 +8,13 @@ import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
+import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
 import java.util.Objects;
 import java.util.concurrent.CountDownLatch;
-import java.util.logging.Logger;
 
 public class PolishCalcClientMain implements QuarkusApplication {
     private static final Integer DEFAULT_PORT = 41000;
@@ -25,20 +26,22 @@ public class PolishCalcClientMain implements QuarkusApplication {
 
     @Override
     public int run(String... args) throws Exception {
-        Integer port = getPortFromArgs(args);
+        final String host = getAddrFromArgs(args);
+        final Integer port = getPortFromArgs(args);
         Bootstrap bootstrap = createClient(new ChannelInboundHandlerAdapter() {
             @Override
             public void channelActive(ChannelHandlerContext ctx) throws Exception {
-                super.channelActive(ctx);
+
             }
 
             @Override
             public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
-                super.channelUnregistered(ctx);
+                log.info("Connection lost, exit.");
+                Quarkus.asyncExit();
             }
         });
-
-        countDownLatch.await();
+        bootstrap.connect(host, port).sync();
+        Quarkus.waitForExit();
         return 0;
     }
 
@@ -58,14 +61,24 @@ public class PolishCalcClientMain implements QuarkusApplication {
     }
 
     private Integer getPortFromArgs(String ... args) {
-        if (args.length > 1) {
+        if (args.length > 2) {
             try {
-                return Integer.parseInt(args[0]);
+                return Integer.parseInt(args[1]);
             } catch (NumberFormatException e) {
                 return DEFAULT_PORT;
             }
         } else {
             return DEFAULT_PORT;
+        }
+    }
+
+    private String getAddrFromArgs(String ... args) {
+        if (args.length >= 1) {
+            return args[0];
+        } else {
+            log.error("Missing host address ");
+            Quarkus.asyncExit(1);
+            throw new IllegalArgumentException("Missing host address");
         }
     }
 }
