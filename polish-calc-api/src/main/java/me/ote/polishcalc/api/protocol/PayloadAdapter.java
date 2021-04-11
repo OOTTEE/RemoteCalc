cbe0dcb07064a0d76d9b05bbb0530e5844e48985
OTE <javier.ote.ote@gmail.com>
2021-04-11 10:24:53 +0200

    Defined nibble payload

---
+++ polish-calc-api/src/main/java/me/ote/polishcalc/api/protocol/PayloadAdapter.java
@@ -0,0 +1,13 @@
+package me.ote.polishcalc.api.protocol;
+
+public class PayloadAdapter {
+
+    public static byte[] compress(String strPayload){
+
+    }
+
+    public static String uncompress(byte[] rawPayload){
+
+    }
+
+}
