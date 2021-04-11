51dbe2c4965ce93d874d46d48b9ca1355a717923
OTE <javier.ote.ote@gmail.com>
2021-04-11 16:42:32 +0200

    Fix bab spaces on PayloadAdapterTest

--- polish-calc-api/src/test/java/me/ote/polishcalc/api/protocol/PayloadAdpaterTest.java
+++ polish-calc-api/src/test/java/me/ote/polishcalc/api/protocol/PayloadAdpaterTest.java
@@ -4,49 +4,54 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import javax.inject.Inject;
+
 @QuarkusTest
 class PayloadAdapterTest {
 
+    @Inject
+    PayloadAdapter payloadAdapter;
+
     @Test
-    public void testCharToNibble1() {
+    public void testCharToNibble1() throws PayloadFormatException {
         String op = "1 2 3 + *";
-        byte[] result = {0x10, 0x20, 0x30, (byte)0xA0, (byte)0xC0};
-        Assertions.assertArrayEquals(result, PayloadAdapter.compress(op));
+        byte[] result = {0x1E, 0x2E, 0x3E, (byte)0xAE, (byte)0xCE};
+        Assertions.assertArrayEquals(result, payloadAdapter.compress(op));
     }
 
     @Test
-    public void testCharToNibble2() {
+    public void testCharToNibble2() throws PayloadFormatException {
         String op = "11 2 3 + *";
-        byte[] result = {0x11, 0x02, 0x03, 0x0A, 0x0C};
-        Assertions.assertArrayEquals(result, PayloadAdapter.compress(op));
+        byte[] result = {0x11, (byte) 0xE2,(byte)  0xE3,(byte)  0xEA,(byte)  0xEC};
+        Assertions.assertArrayEquals(result, payloadAdapter.compress(op));
     }
 
     @Test
-    public void testCharToNibble3() {
+    public void testCharToNibble3() throws PayloadFormatException {
         String op = "1 2 + 5 3 / *";
-        byte[] result = {0x10, 0x20, (byte)0xA0, 0x50, 0x30, (byte)0xD0, (byte)0xC0};
-        Assertions.assertArrayEquals(result, PayloadAdapter.compress(op));
+        byte[] result = {0x1E, 0x2E, (byte)0xAE, 0x5E, 0x3E, (byte)0xDE, (byte)0xCE};
+        Assertions.assertArrayEquals(result, payloadAdapter.compress(op));
     }
 
     @Test
-    public void testNibbleToChar1() {
-        byte[] op = {0x10, 0x20, 0x30, (byte)0xA0, (byte)0xC0};
+    public void testNibbleToChar1() throws PayloadFormatException {
+        byte[] op = {0x1E, 0x2E, 0x3E, (byte)0xAE, (byte)0xCE};
         String result = "1 2 3 + *";
-        Assertions.assertEquals(result, PayloadAdapter.uncompress(op));
+        Assertions.assertEquals(result, payloadAdapter.uncompress(op));
     }
 
     @Test
-    public void testNibbleToChar2() {
-        byte[] op = {0x11, 0x02, 0x03, 0x0A, 0x0C};
+    public void testNibbleToChar2() throws PayloadFormatException {
+        byte[] op = {0x11, (byte) 0xE2, (byte)  0xE3, (byte) 0xEA, (byte) 0xEC};
         String result = "11 2 3 + *";
-        Assertions.assertEquals(result, PayloadAdapter.uncompress(op));
+        Assertions.assertEquals(result, payloadAdapter.uncompress(op));
     }
 
     @Test
-    public void testNibbleToChar3() {
-        byte[] op = {0x10, 0x20, (byte)0xA0, 0x50, 0x30, (byte)0xD0, (byte)0xC0};
+    public void testNibbleToChar3() throws PayloadFormatException {
+        byte[] op = {0x1E, 0x2E, (byte)0xAE, 0x5E, 0x3E, (byte)0xDE, (byte)0xCE};
         String result = "1 2 + 5 3 / *";
-        Assertions.assertEquals(result, PayloadAdapter.uncompress(op));
+        Assertions.assertEquals(result, payloadAdapter.uncompress(op));
     }
 
 }
